a7159be829e7a9ac56af7970a3feee7698bbdfb0
chacuavip10 <chacuavip10@gmail.com>
2026-04-18 10:25:58 +0700

    new-branch, only use icon (font) instead of image
    Done RTC Sync icon
    Pending signal bar
    
    Pending add style to icon (red when no_sig or sig_bad, yellow -> sig_morderate, green -> good/execllent)
    done swap image to icon
    
    add color to signal
    
    fix bug in no_signal still green when gps_timeout
    
    fix bug no_sig color when fix->nofix
    
    ui_file

--- main/ui/images.c
+++ main/ui/images.c
@@ -1,11 +1,5 @@
 #include "images.h"
 
-const ext_img_desc_t images[7] = {
-    { "no-signal_48px", &img_no_signal_48px },
-    { "satlite_white_40px", &img_satlite_white_40px },
-    { "signal_ex_48px", &img_signal_ex_48px },
-    { "signal_good_48px", &img_signal_good_48px },
-    { "signal_moderate_48px", &img_signal_moderate_48px },
-    { "signal_poor_48px", &img_signal_poor_48px },
-    { "sync_48px", &img_sync_48px },
+const ext_img_desc_t images[1] = {
+    0
 };
